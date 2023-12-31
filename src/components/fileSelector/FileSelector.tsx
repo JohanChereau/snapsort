22c337ae052c2caee1f445d7d2c8a50aa6e270e5
Johan Chereau <johanchereau2@gmail.com>
2023-12-31 16:21:25 +0100

    feat: - Setting up file path selection with a dialog window. - Creation of a FileSelector indicator to show the user that the folder has been correctly selected, and display of its name. - Modification of the main and preload files to set up communication between front-end and back-end.

--- src/components/fileSelector/FileSelector.tsx
+++ src/components/fileSelector/FileSelector.tsx
@@ -1,12 +1,16 @@
-import './FileSelector.scss';
 import folderIcon from '@/assets/icons/folder.svg';
 import browseFolderIcon from '@/assets/icons/browse-folder.svg';
+import checkIcon from '@/assets/icons/check.svg';
+import './FileSelector.scss';
 
 type FileSelectorProps = {
   title?: string;
   subtitle?: string;
   instructions?: string;
   actionButtonText?: string;
+  onFolderSelected?: (folder: string) => void;
+  isFolderValid: boolean;
+  selectedFolder: string | null;
 };
 
 const FileSelector = ({
@@ -14,9 +18,36 @@ const FileSelector = ({
   subtitle,
   instructions,
   actionButtonText,
+  onFolderSelected,
+  isFolderValid,
+  selectedFolder,
 }: FileSelectorProps) => {
+
+  const handleBrowseClick = async () => {
+    try {
+      const result = await window.electron.dialog.showOpenDialog({
+        properties:['openDirectory'],
+      });
+
+      if(!result.canceled && result.filePaths.length > 0) {
+        const selectedFolder = result.filePaths[0];
+
+        if(onFolderSelected) {
+          onFolderSelected(selectedFolder);
+        }
+      }
+    } catch(error) {
+      console.error('An error has occurred while opening the dialog window.');
+    }
+  }
+
   return (
     <div className="file-selector-container">
+
+      {
+        isFolderValid && <img src={checkIcon} alt="Checked icon : the folder is valid" className='file-selector__checked-icon'/>
+      }
+
       <h3 className="file-selector__title">{title || 'Title'}</h3>
       <p className="file-selector__subtitle darken-text">
         {subtitle || 'Subtitle'}
@@ -28,10 +59,10 @@ const FileSelector = ({
           alt="Folder icon"
           className="drag-and-drop-area__icon"
         />
-        <button className="button bg-primary drag-and-drop-area__browse-button">
+        <button className="button bg-primary drag-and-drop-area__browse-button" onClick={() => handleBrowseClick()}>
           <img src={browseFolderIcon} alt="" className="button__icon" />
           <span className="button__text">
-            {actionButtonText || 'Browse folder'}
+            {selectedFolder ? 'Modify folder' : (actionButtonText ? actionButtonText : 'Browse folder')}
           </span>
         </button>
 
@@ -40,7 +71,7 @@ const FileSelector = ({
         </p>
       </div>
 
-      <p className="file-selector__selected-filepath">...</p>
+      <p className="file-selector__selected-filepath">{selectedFolder || '...'}</p>
     </div>
   );
 };
--- src/pages/sortingTool/SortingToolPage.tsx
+++ src/pages/sortingTool/SortingToolPage.tsx
@@ -1,20 +1,45 @@
+import { useState } from 'react';
 import './SortingToolPage.scss';
 import FileSelector from 'components/fileSelector/FileSelector';
 
 const SortingToolPage = () => {
+
+  const [sourceFolder, setSourceFolder] = useState<string | null>(null);
+  const [exportFolder, setExportFolder] = useState<string | null>(null);
+
+  console.log(sourceFolder);
+  console.log(exportFolder);
+
   return (
     <main>
-        <section className="sorting-tool-section">
-            <div className="file-selectors-container container">
-                <FileSelector title='Upload your photos' subtitle='Select your photo folder to sort.' instructions='or drag & drop your folder here.' actionButtonText='Browse folder'/>
-                <FileSelector title='Export your photos' subtitle='Select a folder for exporting your sorted photos.' instructions='or drag & drop your folder here.' actionButtonText='Browse folder'/>
-            </div>
+      <section className="sorting-tool-section">
+        <div className="file-selectors-container container">
+          <FileSelector
+            title="Upload your photos"
+            subtitle="Select your photo folder to sort."
+            instructions="or drag & drop your folder here."
+            actionButtonText="Browse folder"
+            onFolderSelected={(folder) => setSourceFolder(folder)}
+            isFolderValid={sourceFolder ? true : false}
+            selectedFolder={sourceFolder}
+          />
+          <FileSelector
+            title="Export your photos"
+            subtitle="Select a folder for exporting your sorted photos."
+            instructions="or drag & drop your folder here."
+            actionButtonText="Browse folder"
+            onFolderSelected={(folder) => setExportFolder(folder)}
+            isFolderValid={exportFolder ? true : false}
+            selectedFolder={exportFolder}
+          />
+        </div>
 
-            <button className="button bg-primary sorting-tool__sort-button">Sort</button>
-
-        </section>
+        <button className="button bg-primary sorting-tool__sort-button">
+          Sort
+        </button>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default SortingToolPage
+export default SortingToolPage;
